1ef6058f01b2cd98ab6c583849fcf18f53648c11
avinash275 <avinashvi275@gmail.com>
2024-04-01 17:09:55 +0530

    Refactor: add folder structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
+import './navbar.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="navbar">
+        <a href="">Home</a>
+        <a href="">About Us</a>
+        <a href="">Contact</a>
+        <a href="">Sign In</a>
+      </div>
+      <div className="container">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam tenetur voluptas assumenda eius at molestiae doloremque delectus facilis dolorem odit adipisci fugit suscipit ut vitae dolore accusamus nobis, reprehenderit tempore. Obcaecati optio dolor, odio eligendi nam inventore ipsa accusamus, delectus magnam excepturi voluptatem placeat laudantium nobis cupiditate illo officiis laborum expedita in qui provident? Ab provident temporibus asperiores laudantium ut molestiae, mollitia dolor. Fugiat, totam nisi ab repellat quisquam nobis minus optio voluptatibus, asperiores fuga, voluptate velit! Et rerum officiis veniam numquam accusantium? Minus dignissimos numquam dolorum possimus? Unde similique minima nihil culpa qui iure asperiores exercitationem voluptatum fugit vel facere optio repudiandae sapiente, autem ex maiores delectus doloribus omnis laudantium natus itaque at distinctio! Quo, deleniti sequi fugiat consectetur ex officia culpa mollitia vero rem sed nemo beatae ipsa quos, et ad a libero maxime, corporis provident! Hic animi amet unde dolorum ad placeat molestias repellat deserunt, excepturi debitis sint ducimus vitae natus consectetur quaerat officiis quam! Recusandae, cumque laboriosam nemo fugiat doloribus accusantium facere. Exercitationem, molestias quibusdam, velit labore dolore illum non quae minima nisi eum enim dolorem quam praesentium quo veniam voluptatum tempore eos ab. Quas autem quis iure est! Ducimus sed necessitatibus aliquid at adipisci corporis dolorem neque? Voluptatibus, in esse! Natus impedit porro ad cum suscipit sequi, nobis laudantium nihil! Quaerat harum omnis ex nulla nostrum illum reprehenderit totam, aliquid ab ut libero in, corrupti, nisi expedita error unde accusamus! Illo quis obcaecati ut praesentium repellat modi neque non vel adipisci harum rerum ea iusto earum, repellendus suscipit, ad eos dolore reiciendis consectetur alias id reprehenderit sunt recusandae quaerat. Excepturi quibusdam consectetur, ut adipisci commodi fugit asperiores, deleniti quisquam non esse possimus. Earum sint qui reprehenderit ex illum recusandae, accusamus ratione distinctio ea odit magni fuga corrupti! Vitae esse, laborum dolorum sed et porro iste omnis. Impedit sunt voluptatibus, eum sint sit, praesentium aut harum fugiat aliquam optio, nostrum similique? Eos, obcaecati? Possimus ex eaque atque accusantium maxime. Quibusdam atque omnis veritatis earum, facere quo, tempora vero quas iure, minima blanditiis pariatur! Rerum, odio minima?</p>
+      </div>
+      <div className="footer">
+        <p>&#169; copyrights reserved 2023</p>
+        
+      </div>
     </div>
   );
 }
